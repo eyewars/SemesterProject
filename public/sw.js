b59ef59775a56e4d62f32270994122e83844a69c
eyewars <eyewars@hotmail.com>
2024-03-15 15:08:53 +0100

    added service worker

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const cacheName = "v1";
+const assets = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/js/app.js",
+    "/js/templateManager.mjs",
+    "/js/draw.mjs",
+    "/js/clientConnect.mjs",
+    "/js/fetchHandler.mjs",
+    "/js/localStorageHandler.mjs",
+    "/js/socketEmitHandler.mjs",
+    "/js/getInfo.mjs",
+    "/manifest.json",
+    "/images/icon.png",
+];
+
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(resources);
+};
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    return fetch(request);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache(assets)
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
+})
